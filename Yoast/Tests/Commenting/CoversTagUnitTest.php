25ed1bdf10ddeb39da4dec553ef98f1eddd19144
jrfnl <jrfnl@users.noreply.github.com>
2019-05-08 04:12:05 +0200

    YoastCS: :sparkles: add new `Yoast.Commenting.CoversTag` sniff
    This new sniff checks the following:
    * That the contents of PHPUnit `@covers` contains a supported annotation.
       Includes a fixer for some common errors.
    * That union/intersect annotations aren't used.
        If found, the annotation will be split in separate `@covers` tags.
    * That a docblock doesn't contain both a `@covers` tag as well as a `@coversNothing` tag.
    * That a docblock doesn't contain multiple `@coversNothing` tags.
        If found and none or only one of the annotations contains a comment, the superfluous tags will be removed.
    * That a docblock doesn't contain duplicate `@covers` tags.
        If found, the superfluous tags will be removed.
    
    Includes unit tests.
    
    Note: a future enhancement to this sniff could be to add support for detecting class-level `@coversDefaultClass` annotations and verifying that the default class name is not used in method-level `@covers` tags, including auto-fixing those.

---
+++ Yoast/Tests/Commenting/CoversTagUnitTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace YoastCS\Yoast\Tests\Commenting;
+
+use PHP_CodeSniffer\Tests\Standards\AbstractSniffUnitTest;
+
+/**
+ * Unit test class for the CoversTag sniff.
+ *
+ * @package Yoast\YoastCS
+ *
+ * @since   1.3.0
+ *
+ * @covers  YoastCS\Yoast\Sniffs\Commenting\CoversTagSniff
+ */
+class CoversTagUnitTest extends AbstractSniffUnitTest {
+
+	/**
+	 * Returns the lines where errors should occur.
+	 *
+	 * @return array <int line number> => <int number of errors>
+	 */
+	public function getErrorList() {
+		return array(
+			34  => 1,
+			35  => 1,
+			36  => 1,
+			37  => 1,
+			38  => 1,
+			39  => 1,
+			40  => 1,
+			47  => 1,
+			48  => 1,
+			49  => 1,
+			50  => 2,
+			57  => 1,
+			58  => 1,
+			59  => 1,
+			66  => 1,
+			67  => 1,
+			68  => 1,
+			75  => 1,
+			76  => 1,
+			77  => 1,
+			84  => 1,
+			91  => 1,
+			101 => 1,
+			114 => 1,
+			127 => 1,
+			140 => 1,
+			150 => 1,
+			151 => 1,
+		);
+	}
+
+	/**
+	 * Returns the lines where warnings should occur.
+	 *
+	 * @return array <int line number> => <int number of warnings>
+	 */
+	public function getWarningList() {
+		return array();
+	}
+}

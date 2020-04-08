e709263bd0ae8d8e9cf5028002a710969cc7fbaa
jrfnl <jrfnl@users.noreply.github.com>
2020-04-08 04:25:33 +0200

    CS: use *un*qualified names in docblocks

--- Yoast/Sniffs/WhiteSpace/FunctionSpacingSniff.php
+++ Yoast/Sniffs/WhiteSpace/FunctionSpacingSniff.php
@@ -50,9 +50,8 @@ class FunctionSpacingSniff extends Squiz_FunctionSpacingSniff {
 	/**
 	 * Processes this test, when one of its tokens is encountered.
 	 *
-	 * @param \PHP_CodeSniffer\Files\File $phpcsFile The file being scanned.
-	 * @param int                         $stackPtr  The position of the current token
-	 *                                               in the stack passed in $tokens.
+	 * @param File $phpcsFile The file being scanned.
+	 * @param int  $stackPtr  The position of the current token in the stack passed in $tokens.
 	 *
 	 * @return void|int Optionally returns stack pointer to skip to.
 	 */

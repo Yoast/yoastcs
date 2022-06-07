c2526127ca5dde7b3e22dc7c9e4e51fad2a38c5e
Juliette <663378+jrfnl@users.noreply.github.com>
2022-06-07 14:02:54 +0200

    Update the sniff skeleton

--- Yoast/Sniffs/PHP/ModernClassRefsSniff.php
+++ Yoast/Sniffs/PHP/ModernClassRefsSniff.php
@@ -1,3 +1,48 @@
 <?php
+/**
+ * PHPCSExtra, a collection of sniffs and standards for use with PHP_CodeSniffer.
+ *
+ * @package   PHPCSExtra
+ * @copyright 2020 PHPCSExtra Contributors
+ * @license   https://opensource.org/licenses/LGPL-3.0 LGPL3
+ * @link      https://github.com/PHPCSStandards/PHPCSExtra
+ */
 
-//ModernClassRefs
+namespace PHPCSExtra\Universal\Sniffs\PHP;
+
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Sniffs\Sniff;
+
+/**
+ * @todo Explain what the sniff does.
+ */
+class ModernClassRefsSniff implements Sniff
+{
+  
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @since 1.0.0
+     *
+     * @return array
+     */
+    public function register()
+    {
+        return ...;
+    }
+
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @since 1.0.0
+     *
+     * @param \PHP_CodeSniffer\Files\File $phpcsFile The file being scanned.
+     * @param int                         $stackPtr  The position of the current token
+     *                                               in the stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(File $phpcsFile, $stackPtr)
+    {
+    }
+}
